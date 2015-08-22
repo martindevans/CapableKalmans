ea9991b72714e0abeaef1c445e3c01ffde4d5651
Martin Evans <martindevans@gmail.com>
2015-08-22 03:54:06 +0100

    Initial commit
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ CapableKalmans/Kalman.fs
@@ -0,0 +1,28 @@
+﻿namespace CapableKalmans
+
+module Kalman =
+
+    type FilterParameters = {
+        // The standard deviation of measurement error
+        R: float
+    }
+
+    type FilterState = {
+        Gain: float;
+        ErrorCovariance: float;
+        Estimation: float;
+    }
+
+    let DefaultFilterState () : FilterState =
+        { Gain = 1.0; ErrorCovariance = 1.0; Estimation = 0.0; }
+    
+    let TimeUpdate (filter:FilterParameters, state:FilterState) : FilterState =
+        state
+
+    let MeasurementUpdate (filter:FilterParameters, state:FilterState, measurement:float) : FilterState = 
+        let k = state.ErrorCovariance / (state.ErrorCovariance + filter.R)
+        {
+            Gain = k;
+            Estimation = state.Estimation + k * (measurement - state.Estimation)
+            ErrorCovariance = (1.0 - k) * state.ErrorCovariance
+        }
---
+++ Console/Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open CapableKalmans
+open System
+open System.Numerics;
+
+[<EntryPoint>]
+let main argv = 
+
+    //Create a new Kalman filter with std dev 0.5
+    let p : Kalman.FilterParameters = {
+        R = 0.5
+    }
+
+    // Initialise default filter state
+    let mutable k = Kalman.DefaultFilterState ()
+
+    //Random number generator (this doesn't generate normally distributed values, so technically we're misusing the kalman filter here)
+    let r = new Random()
+
+    //Feed in random value between 0.5 and 1.5
+    //Over time the filter will converge on 1.0 being the *true* underlying value
+    while true do
+        k <- Kalman.MeasurementUpdate (p, k, 0.5 + r.NextDouble ())
+        k <- Kalman.TimeUpdate (p, k)
+        printfn "%O" k.Estimation
+
+    // Program return code (whatever)
+    0
